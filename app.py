b44f9f9727ee76133ca0e562437e7a807e4e7c3e
kelvinocampo <145300425+kelvinocampo@users.noreply.github.com>
2024-02-15 12:37:22 -0500

    img app.py

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import json
 import google.generativeai as genai 
+import PIL.Image
+import flet as ft
+
+#crea la interfaz
+def main(page : ft.Page):
+    pass
+    
+#inicializa la interfaz
+ft.app(target=main)
 
 def obtener_api_key ():
     with open ('config.json', 'r') as f:
@@ -10,11 +19,15 @@ def obtener_api_key ():
 GOOGLE_API_KEY = obtener_api_key()
 genai.configure(api_key=GOOGLE_API_KEY)
 
+#identifica la imagen
+img = PIL.Image.open('image.jpg')
+
 #crear modelo
-model = genai.GenerativeModel('gemini-pro')
+model = genai.GenerativeModel('gemini-pro-vision')
 
 #generar contenido
-response = model.generate_content('¿Que es la vida?')
+response = model.generate_content(["Write a short, engaging blog post based on this picture. It should include a description of the meal in the photo and talk about my journey meal prepping.", img])
+response.resolve()
 
 #imprimir
 print(response.text)
